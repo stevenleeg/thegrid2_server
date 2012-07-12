27423181445f0490008aa302815f2cb65fd63271
Steve Gattuso <stevenleeg@gmail.com>
2012-07-12 16:59:23 -0400

    Adds some general events

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var WebSocketServer = require("websocket").server;
 var Express = require("express");
 var User = require("./model/user").User;
+var Grid = require("./model/grid").Grid;
+var EventManager = require("./eventmanager").EventManager;
 
 var app = Express.createServer();
 app.listen(8080);
@@ -9,22 +11,34 @@ var srv = new WebSocketServer({
     httpServer: app
 });
 
+// Some general events
+EventManager.on("g.getGrids", function(user, data) {
+    var grids = Grid.getGrids();
+
+    for(var i in grids) {
+        user.trigger("m.newGrid", grids[i]);
+    }
+});
+EventManager.on("g.ping", function(user, data) {
+    user.trigger("ping");
+});
+
 srv.on("request", function(req) {
     try {
         var connection = req.accept("grid-1.0", req.origin);
     } catch(e) {
-        request.reject();
+        req.reject();
         return;
     }
     connection.user = new User(connection);
     console.log("[usr:" + connection.user.id + "] User connected");
 
     connection.on("message", function(message) {
-        console.log("[usr:" + connection.user.id + "] Called: " + JSON.parse(message.utf8Data)['f']);
-        connection.user.on_message(message);
+        var msg = JSON.parse(message.utf8Data);
+        EventManager.trigger(msg.e, connection.user, msg.data);
     });
+
     connection.on("close", function() {
-        console.log("[usr:" + connection.user.id + "] User disconnected");
         connection.user.remove();
     });
 });
